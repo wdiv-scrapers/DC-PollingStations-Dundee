0f521502a3f3bc405d7661bbc41916d2e5c763eb
GeoWill <roper.william@gmail.com>
2021-03-11 13:56:34 +0000

    Update council id

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from dc_base_scrapers.geojson_scraper import RandomIdGeoJSONScraper
 
 stations_url = "http://inspire.dundeecity.gov.uk/geoserver/inspire/wms?service=WFS&version=1.3.0&request=GetFeature&typeNames=inspire%3ASV_POLLING_STATIONS&outputFormat=json&srsName=EPSG%3A4326&sortBy=OBJECTID"
 districts_url = "http://inspire.dundeecity.gov.uk/geoserver/inspire/wms?service=WFS&version=1.3.0&request=GetFeature&typeNames=inspire%3ASV_POLLING_DISTRICTS&outputFormat=json&srsName=EPSG%3A4326&sortBy=OBJECTID"
-council_id = 'S12000042'
+council_id = 'DND'
 
 
 stations_scraper = RandomIdGeoJSONScraper(stations_url, council_id, 'utf-8', 'stations')
