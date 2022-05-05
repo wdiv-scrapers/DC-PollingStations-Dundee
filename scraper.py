24172e30a4d2af398d3ee203531f1ab459de50fb
Will Roper <GeoWill@users.noreply.github.com>
2022-05-05 15:48:34 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 from dc_base_scrapers.geojson_scraper import RandomIdGeoJSONScraper
 
 root_url="https://dservices.arcgis.com/GlZ1P6ksdiXNYhvC/arcgis/services/Dundee_Polling_Districts_and_Polling_Stations/WFSServer"
-stations_url = "{root_url}?outputFormat=GEOJSON&request=GetFeature&service=wfs&typename=Polling_Stations".format(root_url=root_url)
-districts_url = "{root_url}?outputFormat=GEOJSON&request=GetFeature&service=wfs&typename=Polling_Districts".format(root_url=root_url)
+stations_url = "{root_url}?srsname=EPSG:27700&outputFormat=GEOJSON&request=GetFeature&service=wfs&typename=Polling_Stations".format(root_url=root_url)
+districts_url = "{root_url}?srsname=EPSG:27700&outputFormat=GEOJSON&request=GetFeature&service=wfs&typename=Polling_Districts".format(root_url=root_url)
 council_id = 'DND'
 
 
